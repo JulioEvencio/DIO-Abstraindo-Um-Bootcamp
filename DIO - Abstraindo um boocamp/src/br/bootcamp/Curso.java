16ad25f860c556aedf4de83d218f6f2521fd21fd
JulioEvencio <julioevencio@acad.ifma.edu.br>
2022-01-29 13:15:21 -0300

    Classe do curso foi implementada

---
+++ DIO - Abstraindo um boocamp/src/br/bootcamp/Curso.java
@@ -0,0 +1,29 @@
+package br.bootcamp;
+
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return Conteudo.XP_PADRAO * cargaHoraria;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+}
